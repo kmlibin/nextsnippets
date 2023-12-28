17830e6d5abb9baae344c23d8c5c0df33957e11e
Kelli Libin <km.putnam2@gmail.com>
2023-12-28 14:12:08 -0700

    fetch and show the snippets from db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import { db } from "./db";
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
+  //snippets will be an array of objects
 
-export default function Home() {
-  return (
-  <div>HomePage</div>
-  )
+  const renderedSnippets = snippets.map((snippet) => {
+    return <div key={snippet.id}>{snippet.title}</div>;
+  });
+  return <div>{renderedSnippets}</div>;
 }
