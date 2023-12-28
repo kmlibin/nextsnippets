94bf9a34e10419d36ee5055d45b2d92faa6a2cd7
Kelli Libin <km.putnam2@gmail.com>
2023-12-28 15:01:21 -0700

    add edit and delete buttons, display title and code

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -10,14 +10,27 @@ interface SnippetShowPageProps {
 }
 
 export default async function SnippetShowPage(props: SnippetShowPageProps) {
-    // await new Promise ((r) => setTimeout(r, 2000)) //checking to make sure load works
+  // await new Promise ((r) => setTimeout(r, 2000)) //checking to make sure load works
   const snippet = await db.snippet.findFirst({
     //turn into number, because stored in db as number
     where: { id: parseInt(props.params.id) },
   });
-//   console.log(snippet);
+  //   console.log(snippet);
   if (!snippet) {
     return notFound();
   }
-  return <div>{snippet.title}</div>;
+  return (
+    <div>
+      <div className="flex m-4 justify-between items-center">
+        <h1 className="text-xl font-bold"> {snippet.title}</h1>
+        <div className="flex gap-4">
+          <button className="p-2 border rounded">Edit</button>
+          <button className="p-2 border rounded">Delete</button>
+        </div>
+      </div>
+      <pre className="p-3 border rounded bg-gray-200 border-gray-200">
+        <code>{snippet.code}</code>
+      </pre>
+    </div>
+  );
 }
