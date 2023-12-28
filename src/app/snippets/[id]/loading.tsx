81a976f1cc651eb36a0241626a4dc49e2188b285
Kelli Libin <km.putnam2@gmail.com>
2023-12-28 14:47:21 -0700

    add a loading screen and not found screen

---
+++ src/app/snippets/[id]/loading.tsx
@@ -0,0 +1,3 @@
+export default function SnippetShowLoading() {
+  return <div>Loading...</div>;
+}
---
+++ src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,7 @@
+export default function SnippetNotFound() {
+  return (
+    <div>
+      <h1 className="text-xl bold">Sorry, no page here</h1>
+    </div>
+  );
+}
