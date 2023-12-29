1ef3cea86e7b6ce7a46cb54c20cb0a9c711f9498
Kelli Libin <km.putnam2@gmail.com>
2023-12-29 10:08:15 -0700

    create server comp to fetch and edit code snip

---
+++ src/app/snippets/[id]/edit/page.tsx
@@ -0,0 +1,30 @@
+import { db } from "@/app/db";
+import { notFound } from "next/navigation";
+import Editor from '@monaco-editor/react'
+
+import SnippetEditForm from "@/app/components/snippet-edit-form";
+
+interface EditSnippetProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function EditSnippet(props: EditSnippetProps) {
+  const id = parseInt(props.params.id);
+  //fetch snippet to edit
+  const snippet = await db.snippet.findFirst({
+    //turn into number, because stored in db as number
+    where: { id },
+  });
+  //   console.log(snippet);
+  if (!snippet) {
+    return notFound();
+  }
+
+  return (
+    <div>
+      <SnippetEditForm snippet={snippet} />
+    </div>
+  );
+}
