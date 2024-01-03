4581bfd62e5f11d7705194238a92cfd29096e127
Kelli Libin <km.putnam2@gmail.com>
2024-01-03 11:26:44 -0700

    edit snippet action, updates code in db

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,14 @@
-'use server';
+"use server";
+import { redirect } from "next/navigation";
 import { db } from "@/app/db";
 
-export async function editSnippet() {
-    console.log('edit snippet called')
+export async function editSnippet(id: number, code: string) {
+  await db.snippet.update({
+    where: { id },
+    //what new info we want to assign to the snippet we found
+    data: { code },
+  });
+
+  //redirect user to show page (snippets/[id])
+  redirect(`/snippets/${id}`);
 }
