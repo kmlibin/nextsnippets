e8ea18803a7ca97b46926739312f958d5a48f9fc
Kelli Libin <km.putnam2@gmail.com>
2024-01-05 14:20:16 -0700

    added error handling for non-validation errors

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -27,40 +27,54 @@ export async function createSnippet(
   formState: { message: string },
   formData: FormData
 ) {
-  //add a new record to our DB (remember, through Prisma)
-  //see MDN - FormData constructor. With an <form> element, submission creates a FormData object populated from form's key/value pairs.
-  //linked to name in
+    //error handling...if anything goes wrong, we end up in the catch statement
+  try {
+    //add a new record to our DB (remember, through Prisma)
+    //see MDN - FormData constructor. With an <form> element, submission creates a FormData object populated from form's key/value pairs.
+    //linked to name in
 
-  //make clear to Next that this is the server action. special directive used by NextJs. If a function has this exact
-  //string at the top of the function, Next treats this as a server action. executed on the server . moved this from a page
-  // "use server";
-  //make sure input is valid, type it with string if needed (if no checks)
-  const title = formData.get("title");
-  const code = formData.get("code");
+    //make clear to Next that this is the server action. special directive used by NextJs. If a function has this exact
+    //string at the top of the function, Next treats this as a server action. executed on the server . moved this from a page
+    // "use server";
+    //make sure input is valid, type it with string if needed (if no checks)
+    const title = formData.get("title");
+    const code = formData.get("code");
 
+    //return a message (via the useFormState hook )
+    if (typeof title !== "string" || title.length < 3) {
+      return {
+        message: "Title must be longer",
+      };
+    }
 
-  //return a message (via the useFormState hook)
-  if (typeof title !== "string" || title.length < 3) {
-    return {
-      message: "Title must be longer",
-    };
+    if (typeof code !== "string" || code.length < 10) {
+      return {
+        message: "Code must be longer",
+      };
+    }
+
+    //take input and create a new record in db. using Prisma client
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    //returns a snippet (const snippet, above) that also has an id prop. we aren't using snippet variable, so not necessary to store
+  
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+        return {
+            message: err.message
+        };
+    } else {
+        return {
+            message: "Something went wrong"
+        }
+    }
+
   }
-
-  if (typeof code !== "string" || code.length < 10) {
-    return {
-      message: "Code must be longer",
-    };
-  }
-
-  //take input and create a new record in db. using Prisma client
-  const snippet = await db.snippet.create({
-    data: {
-      title,
-      code,
-    },
-  });
-  console.log(snippet);
-  //returns a snippet (const snippet, above) that also has an id prop
-  //redirect the user
+      //redirect the user
   redirect("/");
 }
