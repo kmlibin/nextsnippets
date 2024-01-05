0086e063f86d66041071069de88c6edc7a256605
Kelli Libin <km.putnam2@gmail.com>
2024-01-05 13:58:46 -0700

    import delete  snippet, use in client

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -2,6 +2,7 @@
 import { notFound } from "next/navigation";
 import { db } from "@/app/db";
 import Link from "next/link";
+import * as actions from "@/actions";
 
 interface SnippetShowPageProps {
   //params stored as string, remember
@@ -20,13 +21,23 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
   if (!snippet) {
     return notFound();
   }
+
+  //form action assumes that it will send form data, but we are sending a number. need to bind it so it's like a preloaded version 
+  const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id)
   return (
     <div>
       <div className="flex m-4 justify-between items-center">
         <h1 className="text-xl font-bold"> {snippet.title}</h1>
         <div className="flex gap-4">
-          <Link href={`/snippets/${snippet.id}/edit`} className="p-2 border rounded">Edit</Link>
-          <button className="p-2 border rounded">Delete</button>
+          <Link
+            href={`/snippets/${snippet.id}/edit`}
+            className="p-2 border rounded"
+          >
+            Edit
+          </Link>
+          <form action = {deleteSnippetAction}>
+            <button className="p-2 border rounded">Delete</button>
+          </form>
         </div>
       </div>
       <pre className="p-3 border rounded bg-gray-200 border-gray-200">
